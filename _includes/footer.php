21ffbdfef099cd11fdfbfc67e81ca4d9f3698bdd
Josh S <josh.sanger@geekpower.ca>
2018-09-11 15:46:17 -0400

    Removed comments from footer.

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,4 +1,3 @@
-<!--        <script src="--><?php //echo BASE_URL;?><!--/_assets/js/lib/jquery-2.1.4.min.js"></script>-->
         <script src="<?php echo BASE_URL;?>/_assets/js/main.min.js"></script>
         <div class="main-overlay"></div>
         <div class="spinner-overlay"></div>
@@ -6,6 +5,5 @@
             <div class="double-bounce1"></div>
             <div class="double-bounce2"></div>
         </div>
-
     </body>
 </html>
